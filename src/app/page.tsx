a6a0d83b3ba4d56d3621365b926fec363b1bd6dc
marlonchiu <Knight1898@outlook.com>
2025-09-30 16:23:45 +0800

    ✨ feat: 3-3 第一个页面，创建App

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,16 @@
 import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Textarea } from '@/components/ui/textarea'
 
 export default function Home() {
-  return <Button>Click me</Button>
+  return (
+    <div className="h-screen flex justify-center items-center ">
+      <form action="" className="w-full max-w-md flex flex-col gap-4">
+        <h1 className="text-center text-2xl font-bold">Create App</h1>
+        <Input name="name" placeholder="App Name" />
+        <Textarea name="description" placeholder="Description" />
+        <Button>Submit</Button>
+      </form>
+    </div>
+  )
 }
