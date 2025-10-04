6e2b8d95a1e3b7bc144cc3a0eab4455188d6186e
marlonchiu <Knight1898@outlook.com>
2025-10-04 16:10:40 +0800

    ✨ feat: 3-16 tRPC context管理服务

--- src/app/api/trpc/[...trpc]/route.ts
+++ src/app/api/trpc/[...trpc]/route.ts
@@ -1,13 +1,13 @@
 import { fetchRequestHandler } from '@trpc/server/adapters/fetch'
 import type { NextRequest } from 'next/server'
-import { testRouter } from '@/utils/trpc'
+import { testRouter, createTRPCContext } from '@/utils/trpc'
 
 const handler = (request: NextRequest) => {
   return fetchRequestHandler({
     endpoint: '/api/trpc',
     req: request,
     router: testRouter,
-    createContext: () => ({})
+    createContext: createTRPCContext
   })
 }
 
--- src/utils/trpc.ts
+++ src/utils/trpc.ts
@@ -1,11 +1,52 @@
-import { initTRPC } from '@trpc/server'
+import { getServerSession } from '@/server/auth'
+import { initTRPC, TRPCError } from '@trpc/server'
 
-const t = initTRPC.create()
+export async function createTRPCContext() {
+  const session = await getServerSession()
+  // console.log('🚀 ~~~~~ session:', session)
+
+  // if (!session?.user) {
+  //   throw new TRPCError({ code: 'FORBIDDEN' })
+  // }
+
+  return { session }
+}
+
+const t = initTRPC.context<typeof createTRPCContext>().create()
 
 const { router, procedure } = t
 
+const logMiddleware = t.middleware(async ({ ctx, next }) => {
+  const start = Date.now()
+
+  const result = await next()
+
+  console.log('---> Api time:', Date.now() - start)
+
+  return result
+})
+
+const checkLoginMiddleware = t.middleware(async ({ ctx, next }) => {
+  if (!ctx.session?.user) {
+    throw new TRPCError({ code: 'FORBIDDEN' })
+  }
+
+  return next()
+})
+
+const loggedProcedure = procedure.use(logMiddleware)
+
+const protectedProcedure = procedure.use(checkLoginMiddleware)
+
 export const testRouter = router({
-  greeting: procedure.query(() => {
+  greeting: loggedProcedure.query(async ({ ctx }) => {
+    // console.log('🚀 ~ ctx:', ctx)
+    await new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(null)
+      }, 1000)
+    })
+
     return {
       message: 'Hello World'
     }
