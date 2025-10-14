3e022d0e16a0145bfd771cd55425bb08ce3798ef
marlonchiu <Knight1898@outlook.com>
2025-10-14 11:43:30 +0800

    ✨ feat: 7-7 在页面上发请求，修改cors

--- examples/nuxt/app/app.vue
+++ examples/nuxt/app/app.vue
@@ -1,6 +1,20 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <div>Hello World</div>
 </template>
+
+<script setup>
+import { createOpenApiClient } from '@imooc-image-saas/api'
+import { onMounted } from 'vue'
+
+const apiKey = 'asd' // 假的模拟的api key
+const apiClient = createOpenApiClient({ apiKey })
+
+onMounted(() => {
+  apiClient.file.createPresignedUrl.mutate({
+    filename: 'Screenshot 2023-06-20 200151.png',
+    contentType: 'image/png',
+    size: 34105,
+    appId: 'c52963e1-dfa2-4e70-8333-04de2dcbbb4b'
+  })
+})
+</script>
