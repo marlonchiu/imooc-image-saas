3fb3745aad978553643c853845143c5ab5812637
marlonchiu <Knight1898@outlook.com>
2025-10-20 17:31:10 +0800

    ✨ feat: 9-1 打开一个不存在的app怎么处理？

--- src/app/dashboard/apps/[id]/page.tsx
+++ src/app/dashboard/apps/[id]/page.tsx
@@ -4,7 +4,7 @@ import Uppy from '@uppy/core'
 import AwsS3 from '@uppy/aws-s3'
 import { useState } from 'react'
 
-import { trpcPureClient } from '@/utils/api'
+import { trpcPureClient, trpcClientReact } from '@/utils/api'
 import { Button } from '@/components/ui/button'
 import { UploadButton } from '@/components/feature/UploadButton'
 import { Dropzone } from '@/components/feature/Dropzone'
@@ -18,6 +18,14 @@ import { Label } from '@/components/ui/label'
 import Link from 'next/link'
 
 export default function AppPage({ params: { id: appId } }: { params: { id: string } }) {
+  const { data: apps, isPending } = trpcClientReact.apps.listApps.useQuery(void 0, {
+    refetchOnReconnect: false,
+    refetchOnWindowFocus: false,
+    refetchOnMount: false
+  })
+
+  const currentApp = apps?.filter((app) => app.id === appId)[0]
+
   const [uppy] = useState(() => {
     const uppy = new Uppy()
     uppy.use(AwsS3, {
@@ -57,62 +65,76 @@ export default function AppPage({ params: { id: appId } }: { params: { id: strin
     setShowDeleted(value)
   }
 
-  return (
-    <div className="mx-auto h-full">
-      <div className="container flex justify-between items-center h-[60px]">
-        {/* <Button
-          onClick={() => {
-            uppy.upload()
-          }}
-        >
-          Upload
-        </Button> */}
-        <div className="flex justify-start items-center">
-          <Button
-            onClick={() => {
-              setOrderBy((current) => ({
-                ...current,
-                order: current?.order === 'asc' ? 'desc' : 'asc'
-              }))
-            }}
-          >
-            Created At {orderBy.order === 'desc' ? <MoveUp /> : <MoveDown />}
-          </Button>
-          <div className="flex items-center space-x-2 ml-2">
-            <Switch id="is-show-deleted" checked={showDeleted} onCheckedChange={onCheckedChange} />
-            <Label htmlFor="is-show-deleted">{showDeleted ? 'Show Deleted' : 'Hide Deleted'}</Label>
-          </div>
-        </div>
-        <div className="flex justify-center gap-2">
-          <UploadButton uppy={uppy}></UploadButton>
-          <Button asChild>
-            <Link href="./new">New App</Link>
-          </Button>
-          <Button asChild>
-            <Link href={`/dashboard/apps/${appId}/setting/storage`}>
-              <Settings></Settings>
-            </Link>
-          </Button>
+  let children: React.ReactNode
+  if (isPending) {
+    children = <div>Loading...</div>
+  } else if (!currentApp) {
+    children = (
+      <div className="flex flex-col mt-10 p-4 border rounded-md max-w-48 mx-auto items-center">
+        <p className="text-lg">App Not Exist</p>
+        <p className="text-sm">Chose another one</p>
+        <div className="flex flex-col gap-4 items-center">
+          {apps?.map((app) => (
+            <Button key={app.id} asChild variant="link">
+              <Link href={`/dashboard/apps/${app.id}`}>{app.name}</Link>
+            </Button>
+          ))}
         </div>
       </div>
+    )
+  } else {
+    children = (
+      <div className="mx-auto h-full">
+        <div className="container flex justify-between items-center h-[60px]">
+          <div className="flex justify-start items-center">
+            <Button
+              onClick={() => {
+                setOrderBy((current) => ({
+                  ...current,
+                  order: current?.order === 'asc' ? 'desc' : 'asc'
+                }))
+              }}
+            >
+              Created At {orderBy.order === 'desc' ? <MoveUp /> : <MoveDown />}
+            </Button>
+            <div className="flex items-center space-x-2 ml-2">
+              <Switch id="is-show-deleted" checked={showDeleted} onCheckedChange={onCheckedChange} />
+              <Label htmlFor="is-show-deleted">{showDeleted ? 'Show Deleted' : 'Hide Deleted'}</Label>
+            </div>
+          </div>
+          <div className="flex justify-center gap-2">
+            <UploadButton uppy={uppy}></UploadButton>
+            <Button asChild>
+              <Link href="./new">New App</Link>
+            </Button>
+            <Button asChild>
+              <Link href={`/dashboard/apps/${appId}/setting/storage`}>
+                <Settings></Settings>
+              </Link>
+            </Button>
+          </div>
+        </div>
 
-      <Dropzone uppy={uppy} className="relative h-[calc(100%-60px)]">
-        {(dragging) => {
-          return (
-            <>
-              {dragging && (
-                <div className="absolute inset-0 bg-secondary/50 z-10 flex justify-center items-center text-3xl">
-                  Drop File Here to Upload
-                </div>
-              )}
+        <Dropzone uppy={uppy} className="relative h-[calc(100%-60px)]">
+          {(dragging) => {
+            return (
+              <>
+                {dragging && (
+                  <div className="absolute inset-0 bg-secondary/50 z-10 flex justify-center items-center text-3xl">
+                    Drop File Here to Upload
+                  </div>
+                )}
 
-              <FileList uppy={uppy} orderBy={orderBy} appId={appId} showDeleted={showDeleted} />
-            </>
-          )
-        }}
-      </Dropzone>
+                <FileList uppy={uppy} orderBy={orderBy} appId={appId} showDeleted={showDeleted} />
+              </>
+            )
+          }}
+        </Dropzone>
 
-      <UploadPreview uppy={uppy} />
-    </div>
-  )
+        <UploadPreview uppy={uppy} />
+      </div>
+    )
+  }
+
+  return <>{children}</>
 }
