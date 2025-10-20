37b7e7a11710cfbbcadc498423b9afeda8deb37e
marlonchiu <Knight1898@outlook.com>
2025-10-20 17:38:52 +0800

    ✨ feat: 9-2 跳转登录之后回跳到dashboard页面

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,5 @@
-'use client'
-
-import { Button } from '@/components/ui/button'
-import { Input } from '@/components/ui/input'
-import { Textarea } from '@/components/ui/textarea'
-import { UserInfo, SessionProvider } from './UserInfo'
+import { redirect } from 'next/navigation'
 
 export default function Home() {
-  return (
-    <div className="h-screen flex justify-center items-center ">
-      <form action="" className="w-full max-w-md flex flex-col gap-4">
-        <h1 className="text-center text-2xl font-bold">Create App</h1>
-        <Input name="name" placeholder="App Name" />
-        <Textarea name="description" placeholder="Description" />
-        <Button type="submit">Submit</Button>
-      </form>
-      <SessionProvider>
-        <UserInfo></UserInfo>
-      </SessionProvider>
-    </div>
-  )
+  return redirect('/dashboard')
 }
